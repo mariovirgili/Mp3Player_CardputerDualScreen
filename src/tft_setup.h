02734114eb7e7440e6f49fa6f2a3f0d315c25a7a
mariovirgili <giorgiopiras@gmail.com>
2026-01-15 22:10:47 +0100

    Translated tft_setup.h

--- src/tft_setup.h
+++ src/tft_setup.h
@@ -1,34 +1,34 @@
 #pragma once
 
-// --- Driver esterno 240x320 ---
+// --- External driver 240x320 ---
 #define ILI9341_DRIVER
 
-// --- Usa HSPI/SPI3 per tenere separato dal display interno ---
+// --- Use HSPI/SPI3 to keep separate from internal display ---
 #define USE_HSPI_PORT
 
-// --- Pin esterni Cardputer-Adv ---
+// --- External pins Cardputer-Adv ---
 #define TFT_CS     5
 #define TFT_DC     6
 #define TFT_RST    3
 
-// Spostati su GPIO "alternativi"
+// Moved to "alternative" GPIOs
 #define TFT_SCLK 15
 #define TFT_MOSI 13
 #define TFT_MISO -1  // was 4
 
-// Frequenze conservative (poi puoi alzare)
+// Conservative frequencies (can be increased later)
 #define SPI_FREQUENCY        10000000
 #define SPI_READ_FREQUENCY  6000000
 
-// Niente touch
+// No touch
 #define TOUCH_CS -1
 
-// Se i colori fossero strani, prova a decommentare UNA di queste:
+// If colors are weird, try uncommenting ONE of these:
 #define TFT_RGB_ORDER TFT_BGR
 // #define TFT_INVERSION_ON
 #define TFT_INVERSION_OFF
 
-// --- Fonts (CRITICO per print/println e drawString) ---
+// --- Fonts (CRITICAL for print/println and drawString) ---
 #define LOAD_GLCD
 #define LOAD_FONT2
 #define LOAD_FONT4
